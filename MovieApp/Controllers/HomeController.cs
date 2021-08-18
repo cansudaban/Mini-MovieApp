bc21c0ae27cfe926786d8793a36133f51cd84407
Cansu Daban <cansudaban@hotmail.com>
2021-08-18 20:40:23 +0300

    Add project files.

---
+++ MovieApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Data;
+using MovieApp.Models;
+using System.Linq;
+
+namespace MovieApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+            var movies = MovieRepository.Movies;
+            if (id!=null)
+            {
+                movies = movies.Where(i => i.CategoryId == id).ToList();
+            }
+            return View(movies);
+        }
+        public IActionResult Details(int id)
+        {
+            return View(MovieRepository.GetById(id));
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
